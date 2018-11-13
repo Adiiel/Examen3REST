9cb6161c86ab31a519291fbd5646fa5f7025b87b
Adiel Preciado <adielpreciado@upeu.edu.pe>
2018-11-13 11:41:13 -0500

    nnn

--- app/src/main/java/com/example/adielpreciado/examen3/MainActivity.java
+++ app/src/main/java/com/example/adielpreciado/examen3/MainActivity.java
@@ -6,13 +6,18 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-private EditText user;
+private EditText nombr,apelli,user,clav,correo,telf;
 private Button btnagr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        user =(EditText)findViewById(R.id.nom);
+        nombr=(EditText)findViewById(R.id.Nombr);
+        apelli=(EditText)findViewById(R.id.apelli);
+        user =(EditText)findViewById(R.id.user);
+        clav=(EditText)findViewById(R.id.clave);
+        correo=(EditText)findViewById(R.id.Corre);
+        telf=(EditText)findViewById(R.id.telef);
         btnagr=(Button)findViewById(R.id.btnagr);
     }
 }
