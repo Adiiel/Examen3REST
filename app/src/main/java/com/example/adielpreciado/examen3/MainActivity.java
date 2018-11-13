369b4d48622b45ee8da2ce41172f73f18aa3f5ca
Adiel Preciado <adielpreciado@upeu.edu.pe>
2018-11-13 11:23:23 -0500

    nnn

--- app/src/main/java/com/example/adielpreciado/examen3/MainActivity.java
+++ app/src/main/java/com/example/adielpreciado/examen3/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.adielpreciado.examen3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText user;
+private Button btnagr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user =(EditText)findViewById(R.id.nom);
+        btnagr=(Button)findViewById(R.id.btnagr);
     }
 }
